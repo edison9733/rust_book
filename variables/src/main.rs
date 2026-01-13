5ddae333db62ee8cd8030ea611018551e550a417
EdisonL <you@example.com>
2026-01-13 09:55:29 +0800

    Numeric Operations

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,17 @@
 fn main() {
-    let _x = 2.0; // f64
+    // addition
+    let _sum = 5 + 10;
 
-    let _y: f32 = 3.0; // f32
+    // subtraction
+    let _difference = 95.5 - 4.3;
+
+    // multiplication
+    let _product = 4 * 30;
+
+    // division
+    let _quotient = 56.7 / 32.2;
+    let _truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let _remainder = 43 % 5;
 }
