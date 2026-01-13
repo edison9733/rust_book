210189e0e2154339e494089db55fd09a8ed6b8be
EdisonL <you@example.com>
2026-01-13 13:01:56 +0800

    The Character Type

---
+++ target/debug/deps/variables-edb367f00fd8db71.d
@@ -0,0 +1,5 @@
+/Users/apple/projects/rust_book/variables/target/debug/deps/variables-edb367f00fd8db71.d: src/main.rs
+
+/Users/apple/projects/rust_book/variables/target/debug/deps/variables-edb367f00fd8db71: src/main.rs
+
+src/main.rs:
---
+++ target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/apple/projects/rust_book/variables/target/debug/variables: /Users/apple/projects/rust_book/variables/src/main.rs
